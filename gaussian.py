522c40bd1a21a205487b94cd1d51a69b8e0af3e1
Daniel <Chang254@mail.chapman.edu>
2018-02-16 08:30:22 +0000

    Coded integral and interval functions

--- gaussian.py
+++ gaussian.py
@@ -7,4 +7,14 @@ def g(x):
     value = (1/math.sqrt(2*math.pi))*math.exp(-(x**2)/2)
     return value
 
+def interval(f,a,b,dx):
+    n = (b - a)/dx
+    inter = [f(a+k*dx) for k in range(n+1)]
+    return inter
 
+def integrate(i,dx):
+    for k in range(n):
+        integral = 0
+        rect_area = i(k)*dx
+        integral += rect_area
+    return integral
